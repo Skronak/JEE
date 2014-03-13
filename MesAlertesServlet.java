5b58ce372c79653afd78a38265d6f7f099fe06a5
alioum <alexdarmon06@gmail.com>
2014-03-13 15:01:30 +0100

    Mes alertes

---
+++ MesAlertesServlet.java
@@ -0,0 +1,35 @@
+	package nantes.server;
+
+	import java.io.IOException;
+import java.util.Date;
+import java.util.List;
+
+	import javax.servlet.ServletException;
+import javax.servlet.http.*;
+
+import com.google.appengine.api.datastore.*;
+import com.google.appengine.api.datastore.Query.SortDirection;
+import com.google.appengine.api.users.UserService;
+import com.google.appengine.api.users.UserServiceFactory;
+
+public class MesAlertesServlet extends HttpServlet {
+	
+	    public void doGet(HttpServletRequest req, HttpServletResponse resp) {
+	        try {
+	            DatastoreService datastore = DatastoreServiceFactory.getDatastoreService();
+	            UserService userService = UserServiceFactory.getUserService();
+
+	            // Demande tous les 5 derniers messages triés par date décroissante
+	            Query q = new Query("Alerte").addSort("date", SortDirection.DESCENDING);
+	            List<Entity> results = datastore.prepare(q).asList(FetchOptions.Builder.withLimit(5));
+
+	            req.setAttribute("alertes", results);
+	            this.getServletContext().getRequestDispatcher("/WEB-INF/mesalertes.jsp").forward(req, resp);
+	        } catch (ServletException e) {
+	            e.printStackTrace();
+	        } catch (IOException e) {
+	            e.printStackTrace();
+	        }
+	    }
+
+}
---
+++ mesalertes.jsp
@@ -0,0 +1,35 @@
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<%@ page import="com.google.appengine.api.datastore.*" %>
+<%@ page import="java.util.List" %>
+    <jsp:include page="header.jsp"/>
+
+<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+<title>Mes alertes</title>
+</head>
+<body>
+		<h1>Liste des alértes :</h1>
+		<%
+			List<Entity> alertes = (List<Entity>) request.getAttribute("alertes");
+			for (Entity alerte : alertes) {
+		%>
+		<p>
+			<table>
+				<tr><td><strong><%= alerte.getProperty("auteur") %></strong></td></tr>
+	
+				<tr><td>Adresse:</td> <td><%= alerte.getProperty("adresse") %></td></tr>
+				<tr><td>Date:</td> <td><%= alerte.getProperty("date") %></td></tr>
+				<tr><td>Type:</td> <td><%= alerte.getProperty("type") %></td></tr>
+				<tr><td>Statut:</td> <td><%= alerte.getProperty("statut") %></td></tr>
+				<tr><td>Priorité:</td> <td><%= alerte.getProperty("priorite") %></td></tr>
+				<tr><td>Commentaire:</td> <td><%= alerte.getProperty("commentaire") %></td></tr>
+			</table>	
+		</p>
+		<%
+			}
+		%>
+
+</body>
+</html>
