0d60397b60703efe9eda1ab1717c66774c98b866
Skronak <Gllme.couturier@gmail.com>
2014-04-29 10:24:31 +0200

    Update LesAlertes.java

--- jaidemarue/src/com/rue/servlet/LesAlertes.java
+++ jaidemarue/src/com/rue/servlet/LesAlertes.java
@@ -25,12 +25,24 @@ public class LesAlertes extends HttpServlet {
 	        
 	        String query =  "select from " + Alerte.class.getName() +
 	        				" order by date desc";
-	        
+		//NEW--
+	        String alertes2="";
+		//----
+		
 	        try{           
 	    	        List<Alerte> alertes = (List<Alerte>)pm.newQuery(query).execute();
+	    	
+	    	//NEW--
+	    	// On parcours la liste des alertes que l'on vient de recuperer et on la concatene dans un String avec un séparateur
+	    	        for (Alerte alerte : alertes) {
+		    	    	alertes2=alertes2+alerte.getCoord();
+		    	    	alertes2=alertes2+"|";		    	    	
+		    	}
+			request.setAttribute("alertes2",alertes2);
+	    	//----
 	    	        request.setAttribute("alertes", alertes);
-	            }
-	            finally{
+	    	    
+	            } finally {
 	                pm.close();
 	            }
 	        
